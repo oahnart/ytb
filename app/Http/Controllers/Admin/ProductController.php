2886540239dfc3655d228aa507a1de38cfac2f35
TranHao <tranhao491999@gmail.com>
2020-05-02 21:00:09 +0700

    i

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -83,7 +83,7 @@ class ProductController extends Controller
     public function getDeleteProduct($id)
     {
         Product::destroy($id);
-        $category= Product::find($id);
+        $category= Category::find($id);
         return back();
     }
 }
